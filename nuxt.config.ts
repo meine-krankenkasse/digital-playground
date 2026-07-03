84b8d16f8e2dc86a5d68c43c522182770480ed36
Adrian Popescu <adrianpopescu90@gmail.com>
2026-07-03 14:06:37 +0200

    Add basic authentication configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@kgierke/nuxt-basic-auth'],
 
   devtools: {
     enabled: true
@@ -13,6 +13,10 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: '2025-01-15',
+
+  basicAuth: {
+    enabled: false
+  },
 
   eslint: {
     config: {
